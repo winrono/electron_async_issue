9b5888687ed22b224654ff2aedff199f44e6ebd2
Roman <romanmail.by@gmail.com>
2018-08-12 11:42:03 +0300

    clean up

--- gulpfile.js
+++ gulpfile.js
@@ -10,19 +10,13 @@ gulp.task('html', function() {
 gulp.src( src + '/**/*.html');
 });
 
-gulp.task('css', function() {
-gulp.src( src + '/css/*.css')
-.pipe(concatCss('app.css'))
-.pipe(gulp.dest(app + '/css'));
-});
-
 gulp.task('watch', ['serve'], function() {
 gulp.watch( src + '/js/**/*', ['js']);
 gulp.watch( src + '/css/**/*.css', ['css']);
 gulp.watch([ app + '/**/*.html'], ['html']);
 });
 
-gulp.task('serve', ['html', 'css'], function() {
+gulp.task('serve', ['html'], function() {
 run('electron app/main.js').exec();
 });
 
